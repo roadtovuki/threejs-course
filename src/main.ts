b0e6003ddccd7f50df26bd32c5f9c4f64a265a1a
Mateo Vuković <231365949+roadtovuki@users.noreply.github.com>
2025-11-18 19:29:22 +0100

    Set up a basic scene

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,33 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import {
+  BoxGeometry,
+  Mesh,
+  MeshBasicMaterial,
+  PerspectiveCamera,
+  Scene,
+  WebGLRenderer,
+} from "three";
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+const scene = new Scene();
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+const cubeGeometry = new BoxGeometry(1, 1, 1);
+const cubeMaterial = new MeshBasicMaterial({ color: "red" });
+
+const cubeMesh = new Mesh(cubeGeometry, cubeMaterial);
+
+scene.add(cubeMesh);
+
+const camera = new PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  30
+);
+
+camera.position.z = 5;
+scene.add(camera);
+
+const canvasEl = document.querySelector("canvas.threejs") as Element;
+const renderer = new WebGLRenderer({ canvas: canvasEl });
+
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.render(scene, camera);
