63d0a14026e3eab4b28a6d8dfd26d86c3067aa5b
Mateo Vuković <231365949+roadtovuki@users.noreply.github.com>
2025-11-22 10:42:01 +0100

    Introduce position, group and scale

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
 import "./style.css";
 import {
+  AxesHelper,
   BoxGeometry,
+  Group,
   Mesh,
   MeshBasicMaterial,
   PerspectiveCamera,
   Scene,
+  Vector3,
   WebGLRenderer,
 } from "three";
 import { OrbitControls } from "three/examples/jsm/Addons.js";
@@ -15,8 +18,32 @@ const cubeGeometry = new BoxGeometry(1, 1, 1);
 const cubeMaterial = new MeshBasicMaterial({ color: "red" });
 
 const cubeMesh = new Mesh(cubeGeometry, cubeMaterial);
+cubeMesh.position.y = -1;
+const cubeMesh2 = new Mesh(cubeGeometry, cubeMaterial);
+cubeMesh2.position.x = 2;
+const cubeMesh3 = new Mesh(cubeGeometry, cubeMaterial);
+cubeMesh3.position.x = -2;
 
-scene.add(cubeMesh);
+const group = new Group();
+group.add(cubeMesh);
+group.add(cubeMesh2);
+group.add(cubeMesh3);
+
+group.position.y = 2;
+
+scene.add(group);
+
+// cubeMesh.position.y = 1;
+// cubeMesh.position.z = -1;
+
+// const tempVector = new Vector3(0, 1, -1);
+// cubeMesh.position.copy(tempVector);
+
+// cubeMesh.scale.y = 2;
+// cubeMesh.scale.set(2, 2, 1);
+
+const axesHelper = new AxesHelper(2);
+scene.add(axesHelper);
 
 const camera = new PerspectiveCamera(
   35,
@@ -26,6 +53,11 @@ const camera = new PerspectiveCamera(
 );
 
 camera.position.z = 5;
+camera.position.x = 3;
+camera.position.y = 1;
+
+console.log(cubeMesh.position.distanceTo(camera.position));
+
 scene.add(camera);
 
 const canvasEl = document.querySelector("canvas.threejs") as HTMLElement;
@@ -34,7 +66,7 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 
 const controls = new OrbitControls(camera, canvasEl);
 controls.enableDamping = true;
-controls.autoRotate = true;
+// controls.autoRotate = true;
 
 window.addEventListener("resize", () => {
   camera.aspect = window.innerWidth / window.innerHeight;
