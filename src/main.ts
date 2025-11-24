e06af743bbb9ebd7baa7850e6a94a2c1d8668286
Mateo Vuković <231365949+roadtovuki@users.noreply.github.com>
2025-11-24 19:48:57 +0100

    Analyze material objects

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,13 @@ import "./style.css";
 import {
   AxesHelper,
   BoxGeometry,
+  Color,
+  DoubleSide,
+  Fog,
   Mesh,
   MeshBasicMaterial,
   PerspectiveCamera,
+  PlaneGeometry,
   Scene,
   WebGLRenderer,
 } from "three";
@@ -15,9 +19,34 @@ import { OrbitControls } from "three/examples/jsm/Addons.js";
 const scene = new Scene();
 
 const cubeGeometry = new BoxGeometry(1, 1, 1);
-const cubeMaterial = new MeshBasicMaterial({ color: "red", wireframe: true });
-const cubeMesh = new Mesh(cubeGeometry, cubeMaterial);
+const planeGeometry = new PlaneGeometry(1, 1);
+// const material = new MeshBasicMaterial({
+//   color: "red",
+//   transparent: true,
+//   opacity: 0.5,
+// });
+
+const material = new MeshBasicMaterial();
+material.color = new Color("limeGreen");
+material.transparent = true;
+material.opacity = 0.5;
+material.side = DoubleSide;
+
+const fog = new Fog(0xffffff, 1, 10);
+scene.fog = fog;
+scene.background = new Color(0xffffff);
+
+const cubeMesh = new Mesh(cubeGeometry, material);
+
+const cubeMesh2 = new Mesh(cubeGeometry, material);
+cubeMesh2.position.x = 1.5;
+
+const planeMesh = new Mesh(planeGeometry, material);
+planeMesh.position.x = -1.5;
+
 scene.add(cubeMesh);
+scene.add(cubeMesh2);
+scene.add(planeMesh);
 
 const axesHelper = new AxesHelper(2);
 cubeMesh.add(axesHelper);
