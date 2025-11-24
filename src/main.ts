f1afeb4cb66dacc5bc715262cff102f5d01ae795
Mateo Vuković <231365949+roadtovuki@users.noreply.github.com>
2025-11-24 19:08:49 +0100

    Reset for next lecture

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,7 @@
 import "./style.css";
 import {
   AxesHelper,
-  BufferAttribute,
-  BufferGeometry,
+  BoxGeometry,
   Mesh,
   MeshBasicMaterial,
   PerspectiveCamera,
@@ -13,14 +12,9 @@ import { OrbitControls } from "three/examples/jsm/Addons.js";
 
 const scene = new Scene();
 
-const vertices = new Float32Array([0, 0, 0, 0, 2, 0, 2, 0, 0]);
-const bufferAttribute = new BufferAttribute(vertices, 3);
-
-const geometry = new BufferGeometry();
-geometry.setAttribute("position", bufferAttribute);
-
+const cubeGeometry = new BoxGeometry(1, 1, 1);
 const cubeMaterial = new MeshBasicMaterial({ color: "red", wireframe: true });
-const cubeMesh = new Mesh(geometry, cubeMaterial);
+const cubeMesh = new Mesh(cubeGeometry, cubeMaterial);
 scene.add(cubeMesh);
 
 const axesHelper = new AxesHelper(2);
