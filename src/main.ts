b17d8785b998a38b145411e1f894eabc35503e06
Mateo Vuković <231365949+roadtovuki@users.noreply.github.com>
2025-11-24 23:02:50 +0100

    Introduce textures

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,16 @@ import "./style.css";
 import {
   AmbientLight,
   BoxGeometry,
-  Color,
+  CylinderGeometry,
+  Group,
   Mesh,
-  MeshPhysicalMaterial,
+  MeshBasicMaterial,
   PerspectiveCamera,
   PlaneGeometry,
   PointLight,
   Scene,
+  SphereGeometry,
+  TextureLoader,
   TorusKnotGeometry,
   WebGLRenderer,
 } from "three";
@@ -18,35 +21,52 @@ import { OrbitControls } from "three/examples/jsm/Addons.js";
 
 const scene = new Scene();
 
+const textureLoader = new TextureLoader();
+
 const cubeGeometry = new BoxGeometry(1, 1, 1);
 const torusKnotGeometry = new TorusKnotGeometry(0.5, 0.15, 100, 16);
 const planeGeometry = new PlaneGeometry(1, 1);
+const sphereGeometry = new SphereGeometry(0.5, 32, 32);
+const cylinderGeometry = new CylinderGeometry(0.5, 0.5, 1, 32);
 
-// const material = new MeshStandardMaterial();
-const material = new MeshPhysicalMaterial();
-material.color = new Color("green");
+const textureTest = textureLoader.load("/lava-and-rock_albedo.png");
 
-const cubeMesh = new Mesh(cubeGeometry, material);
+const material = new MeshBasicMaterial();
+material.map = textureTest;
 
-const torusMesh = new Mesh(torusKnotGeometry, material);
-torusMesh.position.x = 1.5;
+const group = new Group();
 
-const planeMesh = new Mesh(planeGeometry, material);
-planeMesh.position.x = -1.5;
+const cube = new Mesh(cubeGeometry, material);
 
-scene.add(cubeMesh);
-scene.add(torusMesh);
-scene.add(planeMesh);
+const knot = new Mesh(torusKnotGeometry, material);
+knot.position.x = 1.5;
+
+const plane = new Mesh(planeGeometry, material);
+plane.position.x = -1.5;
+
+const sphere = new Mesh();
+sphere.geometry = sphereGeometry;
+sphere.material = material;
+sphere.position.y = 1.5;
+
+const cylinder = new Mesh();
+cylinder.geometry = cylinderGeometry;
+cylinder.material = material;
+cylinder.position.y = -1.5;
+
+group.add(cube);
+group.add(knot);
+group.add(plane);
+group.add(sphere);
+group.add(cylinder);
+scene.add(group);
 
 const light = new AmbientLight(0xffffff, 0.4);
 scene.add(light);
 
-const pointLight = new PointLight(0xffffff, 0.7);
+const pointLight = new PointLight(0xffffff, 1.2);
 pointLight.position.set(5, 5, 5);
 scene.add(pointLight);
-
-// const axesHelper = new AxesHelper(2);
-// cubeMesh.add(axesHelper);
 
 const camera = new PerspectiveCamera(
   35,
@@ -57,7 +77,7 @@ const camera = new PerspectiveCamera(
 
 camera.position.z = 5;
 
-console.log(cubeMesh.position.distanceTo(camera.position));
+console.log(cube.position.distanceTo(camera.position));
 
 scene.add(camera);
 
@@ -76,6 +96,12 @@ window.addEventListener("resize", () => {
 });
 
 function renderLoop() {
+  group.children.forEach((child) => {
+    if (child instanceof Mesh) {
+      child.rotation.y += 0.01;
+    }
+  });
+
   controls.update();
   renderer.render(scene, camera);
   requestAnimationFrame(renderLoop);
