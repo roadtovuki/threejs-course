52492839defbaf4585135ea7a8f2a8575611662a
Mateo Vuković <231365949+roadtovuki@users.noreply.github.com>
2025-11-24 19:02:48 +0100

    Introduce buffer geometry

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
 import "./style.css";
 import {
   AxesHelper,
-  BoxGeometry,
+  BufferAttribute,
+  BufferGeometry,
   Mesh,
   MeshBasicMaterial,
   PerspectiveCamera,
@@ -12,9 +13,14 @@ import { OrbitControls } from "three/examples/jsm/Addons.js";
 
 const scene = new Scene();
 
-const cubeGeometry = new BoxGeometry(1, 1, 1);
+const vertices = new Float32Array([0, 0, 0, 0, 2, 0, 2, 0, 0]);
+const bufferAttribute = new BufferAttribute(vertices, 3);
+
+const geometry = new BufferGeometry();
+geometry.setAttribute("position", bufferAttribute);
+
 const cubeMaterial = new MeshBasicMaterial({ color: "red", wireframe: true });
-const cubeMesh = new Mesh(cubeGeometry, cubeMaterial);
+const cubeMesh = new Mesh(geometry, cubeMaterial);
 scene.add(cubeMesh);
 
 const axesHelper = new AxesHelper(2);
